3e0577e9d1e0a153334c85867ed699440de28ce5
esakimedia <apachecordovax@gmail.com>
2022-08-25 18:24:28 +0200

    create tags

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -2,14 +2,32 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Tag;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
 
 class TagController extends Controller
 {
     public function index()
     {
         return Inertia::render('Tags/Index');
+    }
+
+    public function create()
+    {
+        return Inertia::render('Tags/Create');
+    }
+
+    public function store()
+    {
+        Tag::create([
+            'tag_name' => Request::input('tagName'),
+            'slug' => Str::slug(Request::input('tagName'))
+        ]);
+
+        return Redirect::route('admin.tags.index');
     }
 }
