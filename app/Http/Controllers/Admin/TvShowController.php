3b02c548fac435c9d5fd6ffb56a4f46b7bc2742e
esakimedia <apachecordovax@gmail.com>
2022-08-26 17:59:50 +0200

    tv show index page

--- app/Http/Controllers/Admin/TvShowController.php
+++ app/Http/Controllers/Admin/TvShowController.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Http\Controllers\Controller;
+use App\Models\TvShow;
+use Illuminate\Support\Facades\Request;
 
 class TvShowController extends Controller
 {
     public function index()
     {
-        return Inertia::render('TvShows/Index');
+        $perPage = Request::input('perPage') ?: 5;
+
+        return Inertia::render('TvShows/Index', [
+            'tvShows' => TvShow::query()
+                            ->when(Request::input('search'), function($query, $search) {
+                                $query->where('name', 'like', "%{$search}%");
+                            })
+                            ->paginate($perPage)
+                            ->withQueryString(),
+            'filters' => Request::only(['search', 'perPage'])
+        ]);
     }
 }
